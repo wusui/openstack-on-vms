eec481001362c9306df12d08620da5c2883f2567
Warren Usui <warren.usui@inktank.com>
2016-12-16 16:28:30 -0800

    Added comments

--- build_openstack.sh
+++ build_openstack.sh
@@ -1,4 +1,19 @@
 #! /bin/bash -fv
+#
+# Run this script, passing it the name of the machine that you want to install on.
+#
+# This should bring up a machine with 1 controller, 1 compute node, and 3 ceph nodes running
+# as vms on that machine.
+#
+# This whole procedure takes about 2 hours.
+#
+# install_start.sh subscribes to the rhel, openstack, and ceph repos, updates, and then
+#     installs the virtual machine modules
+# install_vms.sh brings up the vms.
+# install_run_undercloud_cmds.sh installs the undercloud
+# rem_introspection.sh performs baremetal introspection inside the vm, and then brings up
+# the overcloud
+#
 ./install_start.sh $1
 sleep 300
 ./install_vms.sh $1
