c349a699d1815d5d02e52022664b6a23101ffc04
Warren Usui <wusui@users.noreply.github.com>
2017-01-17 11:07:34 -0800

    Add edit_virsh.sh calls to rem_introspection.sh
    Add calls to edit_virsh.sh

--- rem_introspection.sh
+++ rem_introspection.sh
@@ -5,4 +5,8 @@
 VM_IP=`cat /tmp/vaddrinfo`
 sudo ssh stack@${VM_IP} /tmp/vmsetup/onvm_pre_introspection.sh
 sudo ssh stack@${VM_IP} /tmp/vmsetup/onvm_post_introspection.sh
+y=`sudo virsh list --all | awk '{print $2}' | grep aardvark | paste -d -s`
+for x in $y; do
+    /tmp/vmsetup/edit_virsh.sh $x
+done
 sudo ssh stack@${VM_IP} /tmp/vmsetup/onvm_create_overcloud.sh
