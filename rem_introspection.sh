d586d78aaa528fbb83d13d007807d625b573c44f
Warren Usui <wusui@users.noreply.github.com>
2017-01-20 23:05:26 -0800

    Update rem_introspection.sh

--- rem_introspection.sh
+++ rem_introspection.sh
@@ -3,11 +3,11 @@
 # Run the pieces to put together the overcloud
 #
 VM_IP=`cat /tmp/vaddrinfo`
-sudo -t ssh stack@${VM_IP} /tmp/vmsetup/onvm_pre_introspection.sh
-sudo -t ssh stack@${VM_IP} /tmp/vmsetup/onvm_post_introspection.sh
+sudo ssh -t stack@${VM_IP} /tmp/vmsetup/onvm_pre_introspection.sh
+sudo ssh -t stack@${VM_IP} /tmp/vmsetup/onvm_post_introspection.sh
 y=`sudo virsh list --all | awk '{print $2}' | grep aardvark | paste -d -s`
 for x in $y; do
     /tmp/vmsetup/edit_virsh.sh $x
 done
 sleep 180
-sudo -t ssh stack@${VM_IP} /tmp/vmsetup/onvm_create_overcloud.sh
+sudo ssh -t stack@${VM_IP} /tmp/vmsetup/onvm_create_overcloud.sh
