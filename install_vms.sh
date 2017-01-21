33dc4d38f671f25ad37dd9d9f0aeca3dfb40b7ee
Warren Usui <wusui@users.noreply.github.com>
2017-01-20 23:01:39 -0800

    Use -t in ssh

--- install_vms.sh
+++ install_vms.sh
@@ -2,7 +2,7 @@
 #
 # Run scripts on the remote site so that the vms get created
 #
-ssh $1 sudo /tmp/vmsetup/make_vms.sh
-ssh $1 sudo /tmp/vmsetup/make_iso.sh $1
-ssh $1 sudo /tmp/vmsetup/ansible_run.sh $1
-ssh $1 sudo /tmp/vmsetup/just_before_we_run_vm.sh
+ssh -t $1 sudo /tmp/vmsetup/make_vms.sh
+ssh -t $1 sudo /tmp/vmsetup/make_iso.sh $1
+ssh -t $1 sudo /tmp/vmsetup/ansible_run.sh $1
+ssh -t $1 sudo /tmp/vmsetup/just_before_we_run_vm.sh
