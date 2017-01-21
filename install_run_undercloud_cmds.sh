54a1969c1174520b746394de7dfdfadeb2d16239
Warren Usui <wusui@users.noreply.github.com>
2017-01-20 23:01:06 -0800

    Use -t in some ssh commands

--- install_run_undercloud_cmds.sh
+++ install_run_undercloud_cmds.sh
@@ -10,6 +10,6 @@ VM_IP=`cat /tmp/vaddrinfo`
 sudo ssh ${VM_IP} sudo /tmp/vmsetup/onvm_mkkeys_wrap.sh
 sudo /tmp/vmsetup/everybody_talks.sh
 HOST=`hostname -s`
-sudo ssh stack@${VM_IP} /tmp/vmsetup/setup-vm.sh $HOST
-sudo ssh stack@${VM_IP} /tmp/vmsetup/setup_certifications.sh
-sudo ssh stack@${VM_IP} /tmp/vmsetup/get_images_and_nameserver.sh
+sudo -t ssh stack@${VM_IP} /tmp/vmsetup/setup-vm.sh $HOST
+sudo -t ssh stack@${VM_IP} /tmp/vmsetup/setup_certifications.sh
+sudo -t ssh stack@${VM_IP} /tmp/vmsetup/get_images_and_nameserver.sh
