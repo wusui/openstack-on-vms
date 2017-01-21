8d8e5bd87b6c09c24f3779b0adc95ab969cdb0c4
Warren Usui <wusui@users.noreply.github.com>
2017-01-20 23:04:36 -0800

    Use sudo -t

--- rem_introspection.sh
+++ rem_introspection.sh
@@ -3,11 +3,11 @@
 # Run the pieces to put together the overcloud
 #
 VM_IP=`cat /tmp/vaddrinfo`
-sudo ssh stack@${VM_IP} /tmp/vmsetup/onvm_pre_introspection.sh
-sudo ssh stack@${VM_IP} /tmp/vmsetup/onvm_post_introspection.sh
+sudo -t ssh stack@${VM_IP} /tmp/vmsetup/onvm_pre_introspection.sh
+sudo -t ssh stack@${VM_IP} /tmp/vmsetup/onvm_post_introspection.sh
 y=`sudo virsh list --all | awk '{print $2}' | grep aardvark | paste -d -s`
 for x in $y; do
     /tmp/vmsetup/edit_virsh.sh $x
 done
 sleep 180
-sudo ssh stack@${VM_IP} /tmp/vmsetup/onvm_create_overcloud.sh
+sudo -t ssh stack@${VM_IP} /tmp/vmsetup/onvm_create_overcloud.sh
